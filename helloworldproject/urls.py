d4283c0aa929520ebf6a6143e00afaed953090d1
AyutoUeda <ayuto.ueda@gmail.com>
2020-12-12 22:41:52 +0900

    master

--- helloworldproject/urls.py
+++ helloworldproject/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from django.urls import path
-from .views import helloworldfunc
-from .views import HelloWorldClass
+from django.urls import path, include
+from django.urls.conf import include
+# from .views import helloworldfunc
+# from .views import HelloWorldClass
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('helloworldurl/', helloworldfunc),
-    path('helloworldurl2/', HelloWorldClass.as_view()),
+    # path('helloworldurl/', helloworldfunc),
+    # path('helloworldurl2/', HelloWorldClass.as_view()),
+    path('', include('helloworldapp.urls'))
 ]
 
 # path('  ', 実行内容)
+#       ↑空欄ならどんな文字列でrequestを送られてきても実行内容を実施
