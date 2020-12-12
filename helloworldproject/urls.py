917a7b126f26a6e538c0aa40d65637e3e8f1ad27
AyutoUeda <ayuto.ueda@gmail.com>
2020-12-12 20:53:28 +0900

    'master'

--- helloworldproject/urls.py
+++ helloworldproject/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from .views import helloworldfunc
+from .views import HelloWorldClass
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('helloworldurl/', helloworldfunc)
+    path('helloworldurl/', helloworldfunc),
+    path('helloworldurl2/', HelloWorldClass.as_view()),
 ]
 
 # path('  ', 実行内容)
--- helloworldproject/views.py
+++ helloworldproject/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def helloworldfunc(request):
   responseobject = HttpResponse('<h1>hello world</h1>')
   return responseobject
 
   # requestオブジェクトを受け取りresponseオブジェクトを返す
+
+
+class HelloWorldClass(TemplateView):
+    template_name = "hello.html"
